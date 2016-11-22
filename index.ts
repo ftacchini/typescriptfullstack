e3c9e0c17855902c6018c8d9a22db06f7fd6cee4
Federico Tacchini <Federico Tacchini>
2016-11-21 23:31:09 -0300

    Creacion del proyecto

---
+++ index.ts
@@ -0,0 +1,11 @@
+import * as express from "express";
+
+var app = express();
+
+app.get('/', function (req: express.Request, res: express.Response) {
+  res.send('Hello World!')
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
